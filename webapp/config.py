d862d4865c61f576a808482541880ea340619f24
willboomitra <will@boomitra.com>
2025-02-22 21:35:42 -0800

    remove config.py

--- webapp/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# Device configuration (GPU or CPU)
-device = "cpu"  # Change to "cuda" if using GPU
-
-# Path for storing models
-models_dir = 'models/'  # Update to your model directory
